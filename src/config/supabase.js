24c035c790f845fc22caf2443786be69fef62228
Crainoe <sjvv07@gmail.com>
2026-06-29 03:59:19 +0200

    fix: Simplify Supabase key assignment for improved readability

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Supabase configuration
 const supabaseUrl = "https://dxnwpkdxwcbeaknjvslx.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrZW5hY3hkZHl3YWZxb29qd3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNjM1MjQsImV4cCI6MjA3NzczOTUyNH0.sl9sb_publishable_cPO4vSyFiPy8k0YRIbR8UA_M7IqIhLeV53FNth7KyIcdBF7hsUn2p1MH7QwZwRdakLeEn4I";
+const supabaseKey = "sb_publishable_cPO4vSyFiPy8k0YRIbR8UA_M7IqIhLe";
 
 // Create Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey);
